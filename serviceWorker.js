b425c975708e90f8f0b3b9ca3e76c87cb92fb399
EzzanieMusa <59683612+EzzanieMusa@users.noreply.github.com>
2020-04-12 14:59:11 +0800

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -29,3 +29,11 @@ self.addEventListener("install", installEvent => {
     })
   )
 })
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
